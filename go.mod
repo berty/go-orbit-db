3d609c409bbf9fc67dd3183b0f31f9a7ff5f602f
Guillaume Louvigny <glouvigny@users.noreply.github.com>
2021-05-11 08:37:06 +0200

    feat: support for encrypted next/refs links in entries (#74)
    Signed-off-by: Guillaume Louvigny <glouvigny@users.noreply.github.com>

--- go.mod
+++ go.mod
@@ -1,26 +1,27 @@
 module berty.tech/go-orbit-db
 
-go 1.13
+go 1.15
 
 require (
-	berty.tech/go-ipfs-log v1.2.6
+	berty.tech/go-ipfs-log v1.3.0
+	github.com/davidlazar/go-crypto v0.0.0-20190912175916-7055855a373f // indirect
 	github.com/golang-collections/go-datastructures v0.0.0-20150211160725-59788d5eb259
 	github.com/golang/snappy v0.0.1 // indirect
 	github.com/ipfs/go-cid v0.0.7
-	github.com/ipfs/go-datastore v0.4.4
+	github.com/ipfs/go-datastore v0.4.5
 	github.com/ipfs/go-ds-leveldb v0.4.2
-	github.com/ipfs/go-ipfs v0.6.0
+	github.com/ipfs/go-ipfs v0.8.0
 	github.com/ipfs/go-ipfs-files v0.0.8
-	github.com/ipfs/go-ipld-cbor v0.0.4
-	github.com/ipfs/interface-go-ipfs-core v0.3.0
-	github.com/libp2p/go-libp2p v0.10.2
-	github.com/libp2p/go-libp2p-core v0.6.1
-	github.com/libp2p/go-libp2p-pubsub v0.3.3
+	github.com/ipfs/go-ipld-cbor v0.0.5
+	github.com/ipfs/interface-go-ipfs-core v0.4.0
+	github.com/libp2p/go-libp2p v0.13.0
+	github.com/libp2p/go-libp2p-core v0.8.5
+	github.com/libp2p/go-libp2p-pubsub v0.4.1
+	github.com/libp2p/go-sockaddr v0.1.0 // indirect
 	github.com/pkg/errors v0.9.1
-	github.com/polydawn/refmt v0.0.0-20190807091052-3d65705ee9f1
-	github.com/stretchr/testify v1.6.1
+	github.com/polydawn/refmt v0.0.0-20201211092308-30ac6d18308e
+	github.com/stretchr/testify v1.7.0
 	go.opentelemetry.io/otel v0.8.0
-	go.uber.org/goleak v1.0.0
-	go.uber.org/zap v1.15.0
-	golang.org/x/tools v0.0.0-20200716134326-a8f9df4c9543 // indirect
+	go.uber.org/goleak v1.1.10
+	go.uber.org/zap v1.16.0
 )
--- tests/replicate_test.go
+++ tests/replicate_test.go
@@ -1,6 +1,7 @@
 package tests
 
 import (
+	"berty.tech/go-ipfs-log/enc"
 	"context"
 	"fmt"
 	"os"
@@ -173,8 +174,12 @@ func testDefaultNodeGenerator(t *testing.T, mn mocknet.Mocknet, i int) (orbitdb.
 	ipfs1 := testingCoreAPI(t, node1)
 	zap.L().Named("orbitdb.tests").Debug(fmt.Sprintf("node%d is %s", i, node1.Identity.String()))
 
+	//logger, _ := zap.NewDevelopment()
+	logger := zap.NewNop()
+
 	orbitdb1, err := orbitdb.NewOrbitDB(ctx, ipfs1, &orbitdb.NewOrbitDBOptions{
 		Directory: &dbPath1,
+		Logger:    logger,
 	})
 	require.NoError(t, err)
 
@@ -359,4 +364,147 @@ func TestReplicationMultipeer(t *testing.T) {
 			time.Sleep(4 * time.Second) // wait some time to let CPU relax
 		}
 	}
+}
+
+func TestLogAppendReplicateEncrypted(t *testing.T) {
+	amount := 2
+	nodeGen := testDefaultNodeGenerator
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*50)
+	defer cancel()
+
+	dbs := make([]orbitdb.OrbitDB, 2)
+	dbPaths := make([]string, 2)
+	mn := testingMockNet(ctx)
+
+	sharedKey, err := enc.NewSecretbox([]byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2})
+	require.NoError(t, err)
+
+	for i := 0; i < 2; i++ {
+		dbs[i], dbPaths[i], cancel = nodeGen(t, mn, i)
+		defer cancel()
+	}
+
+	err = mn.LinkAll()
+	require.NoError(t, err)
+
+	err = mn.ConnectAllButSelf()
+	require.NoError(t, err)
+
+	access := &accesscontroller.CreateAccessControllerOptions{
+		Access: map[string][]string{
+			"write": {
+				dbs[0].Identity().ID,
+				dbs[1].Identity().ID,
+			},
+		},
+	}
+
+	store0, err := dbs[0].Log(ctx, "replication-tests", &orbitdb.CreateDBOptions{
+		Directory:        &dbPaths[0],
+		AccessController: access,
+		SharedKey:        sharedKey,
+	})
+	require.NoError(t, err)
+
+	defer func() { _ = store0.Close() }()
+
+	store1, err := dbs[1].Log(ctx, store0.Address().String(), &orbitdb.CreateDBOptions{
+		Directory:        &dbPaths[1],
+		AccessController: access,
+		SharedKey:        sharedKey,
+	})
+	require.NoError(t, err)
+
+	defer func() { _ = store1.Close() }()
+
+	infinity := -1
+
+	for i := 0; i < amount; i++ {
+		_, err = store0.Add(ctx, []byte(fmt.Sprintf("hello%d", i)))
+		require.NoError(t, err)
+	}
+
+	items, err := store0.List(ctx, &orbitdb.StreamOptions{Amount: &infinity})
+	require.NoError(t, err)
+	require.Equal(t, amount, len(items))
+
+	<-time.After(time.Millisecond * 2000)
+	items, err = store1.List(ctx, &orbitdb.StreamOptions{Amount: &infinity})
+	require.NoError(t, err)
+	require.Equal(t, amount, len(items))
+	require.Equal(t, "hello0", string(items[0].GetValue()))
+	require.Equal(t, fmt.Sprintf("hello%d", amount-1), string(items[len(items)-1].GetValue()))
+}
+
+func TestLogAppendReplicateEncryptedWrongKey(t *testing.T) {
+	amount := 5
+	nodeGen := testDefaultNodeGenerator
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*50)
+	defer cancel()
+
+	dbs := make([]orbitdb.OrbitDB, 2)
+	dbPaths := make([]string, 2)
+	mn := testingMockNet(ctx)
+
+	sharedKey0, err := enc.NewSecretbox([]byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2})
+	require.NoError(t, err)
+
+	sharedKey1, err := enc.NewSecretbox([]byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 3})
+	require.NoError(t, err)
+
+	for i := 0; i < 2; i++ {
+		dbs[i], dbPaths[i], cancel = nodeGen(t, mn, i)
+		defer cancel()
+	}
+
+	err = mn.LinkAll()
+	require.NoError(t, err)
+
+	err = mn.ConnectAllButSelf()
+	require.NoError(t, err)
+
+	access := &accesscontroller.CreateAccessControllerOptions{
+		Access: map[string][]string{
+			"write": {
+				dbs[0].Identity().ID,
+				dbs[1].Identity().ID,
+			},
+		},
+	}
+
+	store0, err := dbs[0].Log(ctx, "replication-tests", &orbitdb.CreateDBOptions{
+		Directory:        &dbPaths[0],
+		AccessController: access,
+		SharedKey:        sharedKey0,
+	})
+	require.NoError(t, err)
+
+	defer func() { _ = store0.Close() }()
+
+	store1, err := dbs[1].Log(ctx, store0.Address().String(), &orbitdb.CreateDBOptions{
+		Directory:        &dbPaths[1],
+		AccessController: access,
+		SharedKey:        sharedKey1,
+	})
+	require.NoError(t, err)
+
+	defer func() { _ = store1.Close() }()
+
+	infinity := -1
+
+	for i := 0; i < amount; i++ {
+		_, err = store0.Add(ctx, []byte(fmt.Sprintf("hello%d", i)))
+		require.NoError(t, err)
+	}
+
+	items, err := store0.List(ctx, &orbitdb.StreamOptions{Amount: &infinity})
+	require.NoError(t, err)
+	require.Equal(t, amount, len(items))
+
+	<-time.After(time.Millisecond * 2000)
+	items, err = store1.List(ctx, &orbitdb.StreamOptions{Amount: &infinity})
+	require.NoError(t, err)
+	require.Equal(t, 0, len(items))
 }
